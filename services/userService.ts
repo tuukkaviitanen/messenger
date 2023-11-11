16bd12aa8b015268e75c4a99ea94ed65841a7070
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2023-11-11 14:29:10 +0200

    Added user creation to userService
    Installed bcrypt for hashing passwords and uuid for ids (temporarily before adding a database)

--- services/userService.ts
+++ services/userService.ts
@@ -1,11 +1,17 @@
+import bcrypt from 'bcrypt';
+import {v4 as uuidv4} from 'uuid';
+import {type UserPublic, type UserCredentials} from '../types';
+
 const users = [
 	{
 		id: '1f234f34f2',
 		username: 'hello',
+		passwordHash: 'f224f234f23f',
 	},
 	{
 		id: '1215f234fef',
 		username: 'world',
+		passwordHash: 'f224f2asdafgaef34f23f',
 	},
 ];
 
@@ -13,6 +19,16 @@ const getAll = () => users;
 
 const getSingle = (id: string) => users.find(u => u.id === id);
 
-const userService = {getAll, getSingle};
+const create = async ({username, password}: UserCredentials): Promise<UserPublic> => {
+	const salt = 10;
+	const passwordHash = await bcrypt.hash(password, salt);
+	const id = uuidv4();
+
+	users.push({id, username, passwordHash});
+
+	return {id, username};
+};
+
+const userService = {getAll, getSingle, create};
 
 export default userService;
