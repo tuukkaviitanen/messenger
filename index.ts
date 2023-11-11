bd0ba48640750815845106c866fc5e5271b1f708
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2023-11-11 13:26:10 +0200

    Added express server
    Added /api/healthz endpoint

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.get('/api/healthz', (req, res) => {
+	res.send('ok');
+});
+
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => {
+	console.log(`Express server listening on port ${port}`);
+});
