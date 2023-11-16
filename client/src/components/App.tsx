082b566fa591a7ec60368df2a2eb11318f51f722
tuukkaviitanen <tuukka.viitanen@gmail.com>
2023-11-16 14:32:45 +0200

    Added non-functional login page
    Also added mui theme and styled login page

---
+++ client/src/components/App.tsx
@@ -0,0 +1,10 @@
+import Login from "./Login";
+
+const App = () => {
+return (
+    <Login/>
+)
+}
+
+export default App;
+
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#22092C',
+    },
+    secondary: {
+      main: '#872341',
+    },
+  },
+});
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
