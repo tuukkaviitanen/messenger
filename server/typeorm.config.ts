abe4784827f0af9cda6bb2c0120ef9d3d9ff8c74
tuukkaviitanen <tuukka.viitanen@gmail.com>
2023-12-02 17:12:26 +0200

    Fixed typeorm.config paths
    Now uses the right entities and migrations when running build directory as well.

--- server/typeorm.config.ts
+++ server/typeorm.config.ts
@@ -1,13 +1,16 @@
 import {DataSource} from 'typeorm';
 import config from './src/utils/config';
+import path from 'node:path';
 
 export default new DataSource({
 	type: 'postgres',
 	url: config.postgresUrl,
 	synchronize: false,
 	logging: true,
-	entities: ['src/entities/**/*.ts'],
-	subscribers: ['src/subscribers/**/*.ts'],
-	migrations: ['src/migrations/**/*.ts'],
+	// Path creation needs to take into account running with typeorm.config.ts AND .js, which are located in different directories.
+	// __dirname means translates into the directory in which typeorm.config is located, as ./ would always translate into project root.
+	entities: [path.join(__dirname, 'src/entities/**/*.{js,ts}')],
+	subscribers: [path.join(__dirname, 'src/subscribers/**/*.{js,ts}')],
+	migrations: [path.join(__dirname, 'src/migrations/**/*.{js,ts}')],
 	migrationsTableName: 'migrations',
 });
